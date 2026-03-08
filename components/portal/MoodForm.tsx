8649f4007c30164092401d62978d44ee8f975ce1
Arnaud Chevallier <arnaud@coachbluewave.com>
2026-03-08 10:01:06 +0000

    Revert "fix: remove sommeil and stress from journal tag suggestions"
    This reverts commit a2738c41ada693616da5424629f154070db5d98c.

--- components/portal/MoodForm.tsx
+++ components/portal/MoodForm.tsx
@@ -6,7 +6,7 @@ import { useLanguage } from "@/lib/i18n/useLanguage";
 import WellnessProgram from "@/components/portal/WellnessProgram";
 import { MOOD_EMOJIS } from "@/lib/constants";
 const TAG_SUGGESTIONS = [
-  "sport", "nutrition", "motivation",
+  "sport", "nutrition", "sommeil", "stress", "motivation",
   "fatigue", "énergie", "progrès", "repos", "méditation",
 ];
 
